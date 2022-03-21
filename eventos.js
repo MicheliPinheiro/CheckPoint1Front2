b30145dc5cf6ca5717ec5f63b7f3e91580a9d9b2
MicheliPinheiro <micheli.pinheiro.teresa@gmail.com>
2022-03-21 16:14:05 -0300

    Mi alterando

--- eventos.js
+++ eventos.js
@@ -1,14 +1,27 @@
-const form = document.querySelector('#SeAmostre-form');
+window.onload = function() {
+    const form = document.querySelector('form');
+    
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+        // Gerenciando o input Nome:
+        const entraNome = document.querySelector('#name');
+        const nome = document.createElement('h3');
+        nome.appendChild(document.createTextNode(entraNome.value));
 
-    const amostrado = document.querySelector('textares');
-    const texto = document.queryTextNode(amostrado.value);
-    const cards = document.querySelector('#cards');
-    const card = document.createElement('section');
-
-    section.appendChild(texto);
-    cards.appendChild(card);
-});
-
+        // Gerenciando o input Interesses:
+        const entraInteresses = document.querySelector('#interesses');
+        const interesses = document.createElement('p');
+        interesses.appendChild(document.createTextNode(entraInteresses.value));
+        
+        // Gerenciando o input url:
+        const entraUrl = document.querySelector('#url');
+        const foto = document.createElement('foto-card');
+        foto.classList.add('foto-card');
+        foto.setAttribute('src', entraUrl.value);
+        
+        //Criando os cards
+                
+        form.reset();
+    });
+}
